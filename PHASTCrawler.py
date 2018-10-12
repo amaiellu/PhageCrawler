9b2aa28cf5f4ace8d46624877274479f0057d4d7
amaiellu <amschaef@live.unc.edu>
2018-10-12 15:00:00 -0400

    First commit--very basic crawler outline

---
+++ PHASTCrawler.py
@@ -0,0 +1,47 @@
+'''
+Created on Oct 29, 2018
+
+@author: amschaef
+'''
+from lxml import html
+import requests
+import pandas as pd
+
+
+
+class PHASTCrawler: 
+    def __init__(self,starting_url,depth,microbe):
+        self.starting_url=starting_url
+        self.root_url=starting_url.rsplit('/',1)[0]
+        self.depth=depth
+        self.microbe=microbe
+        self.bactlist=[]
+        self.phagelist=[]   
+    
+    def crawl(self):
+      
+        return
+
+    def get_bacteria_from_link(self,link,microbe):
+              
+                
+        return
+    
+ 
+    
+    def get_tail_phages(self,bacteria):
+
+        return
+    
+    
+
+        return
+      
+class Bacteria:
+    
+        
+    
+
+class TailPhage:
+   
+        
