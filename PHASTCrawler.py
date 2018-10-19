d6bf550208c93a06e612f6da343fadeebef034c9
amaiellu <amschaef@live.unc.edu>
2018-10-19 10:00:00 -0400

    functions to parse detailed file into individual proteins

--- PHASTCrawler.py
+++ PHASTCrawler.py
@@ -46,6 +46,36 @@ class PHASTCrawler:
     
     
     def get_tail_phages(self,bacteria):
+        soup=self.getSoup(bacteria.detailLink)
+        phagetables=soup.find_all('table')[1:]
+        for table in phagetables:
+            rows=table.find_all('tr')
+            for row in rows:
+                if 'tail' in row.get_text():
+                    tds=row.find_all('td')
+                    info=tds[2].get_text().split(':')
+                    name=info[0]
+                    proteintag=info[1].split(';')[0]
+                    sequenceLink=self.root_url+tds[4].find('a').get('href')
+                    sequence=self.getSoup(sequenceLink).get_text()
+                    sequence= ''.join([i for i in sequence if i not in '[>.|0123456789]']) 
+                    currentphages=self.phagelist
+                    existingphage=next((phage for phage in currentphages if phage.name == name), False)
+                    bacteria.tailPhages.append(name)
+                    if existingphage:
+                        newSequence=Sequence(sequence,proteintag,bacteria.name)
+                        existingphage.sequences.append(newSequence)
+                        if bacteria.name not in existingphage.bactList:
+                            existingphage.bactList.append(bacteria.name)
+                            
+                        
+                    else:
+                        newPhage=TailPhage(name)
+                        newPhage.sequenceLink=sequenceLink
+                        newSequence=Sequence(sequence,proteintag,bacteria.name)
+                        newPhage.sequences.append(newSequence)
+                        newPhage.bactList.append(bacteria.name)
+                        self.phagelist.append(newPhage)
 
         return
     
@@ -54,6 +84,15 @@ class PHASTCrawler:
         
       
     def get_summary_phages(self,bacteria):
+        soup=self.getSoup(bacteria.summaryLink)
+        phage_list=[]
+        phagetable=soup.find_all('table')[0]
+        rows=phagetable.find_all('tr')
+        for row in rows: 
+            if row.find_all('td') != []:
+                phage_list.append(row.find_all('td')[6].get_text())
+        
+        bacteria.summaryPhage=phage_list
         
         return
     
